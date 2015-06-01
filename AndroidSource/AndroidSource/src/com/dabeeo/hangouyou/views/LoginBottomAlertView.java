b93f20cdfdee4bbb4f88f6f64cae16ce15532779
Jung Chang Hwan <shakejj@gmail.com>
2015-06-01 16:19:29 +0900

    회원가입 하단 메세지 숨기는 부분 원활하게 보이도록 수정

--- AndroidSource/AndroidSource/src/com/dabeeo/hangouyou/views/LoginBottomAlertView.java
+++ AndroidSource/AndroidSource/src/com/dabeeo/hangouyou/views/LoginBottomAlertView.java
@@ -16,6 +16,7 @@ public class LoginBottomAlertView extends RelativeLayout
   private Context context;
   private LinearLayout alertContainer;
   private TextView alertMessage;
+  private Handler handler;
   
   
   public LoginBottomAlertView(Context context)
@@ -46,20 +47,23 @@ public class LoginBottomAlertView extends RelativeLayout
   {
     alertContainer.setVisibility(View.VISIBLE);
     alertMessage.setText(msg);
-    
-    new Handler().postDelayed(new Runnable()
-    {
-      @Override
-      public void run()
-      {
-        alertContainer.setVisibility(View.GONE);
-      }
-    }, 2000);
+    handler.removeCallbacks(hideAlertRunnable);
+    handler.postDelayed(hideAlertRunnable, 2000);
   }
+  
+  private Runnable hideAlertRunnable = new Runnable()
+  {
+    @Override
+    public void run()
+    {
+      alertContainer.setVisibility(View.GONE);
+    }
+  };
   
   
   public void init()
   {
+    handler = new Handler();
     int resId = R.layout.view_login_bottom_alert;
     View view = LayoutInflater.from(context).inflate(resId, null);
     view.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
