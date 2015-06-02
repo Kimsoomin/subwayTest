08faae868ca3701767f65a508669352f210eb94c
Jung Chang Hwan <shakejj@gmail.com>
2015-06-02 16:17:33 +0900

    디자인 가이드 - 장소 상세 화면 수정 중

--- AndroidSource/AndroidSource/src/com/dabeeo/hangouyou/views/PlaceDetailHeaderView.java
+++ AndroidSource/AndroidSource/src/com/dabeeo/hangouyou/views/PlaceDetailHeaderView.java
@@ -16,63 +16,63 @@ import com.squareup.picasso.Picasso;
 
 public class PlaceDetailHeaderView extends RelativeLayout
 {
-	private Context context;
-	private TextView imageCount;
-	private ImageView imageView;
-	
-	
-	public PlaceDetailHeaderView(Context context)
-	{
-		super(context);
-		this.context = context;
-		init();
-	}
-	
-	
-	public PlaceDetailHeaderView(Context context, AttributeSet attrs, int defStyle)
-	{
-		super(context, attrs, defStyle);
-		this.context = context;
-		init();
-	}
-	
-	
-	public PlaceDetailHeaderView(Context context, AttributeSet attrs)
-	{
-		super(context, attrs);
-		this.context = context;
-		init();
-	}
-	
-	
-	public void setBean(PlaceDetailBean bean)
-	{
-		if (bean != null)
-			imageCount.setText("+" + Integer.toString(bean.bookmarkCount));
-	}
-	
-	
-	public void init()
+  private Context context;
+  private TextView imageCount;
+  private ImageView imageView;
+  
+  
+  public PlaceDetailHeaderView(Context context)
+  {
+    super(context);
+    this.context = context;
+    init();
+  }
+  
+  
+  public PlaceDetailHeaderView(Context context, AttributeSet attrs, int defStyle)
+  {
+    super(context, attrs, defStyle);
+    this.context = context;
+    init();
+  }
+  
+  
+  public PlaceDetailHeaderView(Context context, AttributeSet attrs)
+  {
+    super(context, attrs);
+    this.context = context;
+    init();
+  }
+  
+  
+  public void setBean(PlaceDetailBean bean)
+  {
+    if (bean != null)
+      imageCount.setText("+" + Integer.toString(bean.bookmarkCount));
+  }
+  
+  
+  public void init()
   {
     int resId = R.layout.view_place_detail_header;
     View view = LayoutInflater.from(context).inflate(resId, null);
     
     imageView = (ImageView) view.findViewById(R.id.imageview);
     imageCount = (TextView) view.findViewById(R.id.image_count);
-    
+    imageCount.setVisibility(View.GONE);
     Picasso.with(context).load("https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f46e842e-c688-460e-a70b-e6a4d30e9885/aimper/store_photos/500/photos/small/Punkt_1.jpg?1426234759").fit().centerCrop().into(
         imageView);
     
     imageView.setOnClickListener(new OnClickListener()
-	{
-		@Override
-		public void onClick(View v)
-		{
-			Intent i = new Intent(context, ImagePopUpJustOneActivity.class);
-			i.putExtra("image_url", "https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f46e842e-c688-460e-a70b-e6a4d30e9885/aimper/store_photos/500/photos/small/Punkt_1.jpg?1426234759");
-			context.startActivity(i);
-		}
-	});
+    {
+      @Override
+      public void onClick(View v)
+      {
+        Intent i = new Intent(context, ImagePopUpJustOneActivity.class);
+        i.putExtra("image_url", "https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_f46e842e-c688-460e-a70b-e6a4d30e9885/aimper/store_photos/500/photos/small/Punkt_1.jpg?1426234759");
+        context.startActivity(i);
+      }
+    });
     addView(view);
   }
 }
