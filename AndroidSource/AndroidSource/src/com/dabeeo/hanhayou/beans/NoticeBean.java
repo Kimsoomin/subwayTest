dafd4bbc61476f50252e3c2d1c905fc3a17c98ea
Jung Chang Hwan <shakejj@gmail.com>
2015-06-22 11:37:45 +0900

    공지사항 API 적용

--- AndroidSource/AndroidSource/src/com/dabeeo/hanhayou/beans/NoticeBean.java
+++ AndroidSource/AndroidSource/src/com/dabeeo/hanhayou/beans/NoticeBean.java
@@ -1,8 +1,65 @@
 package com.dabeeo.hanhayou.beans;
 
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class NoticeBean
 {
+  public int isTop;
+  public String idx;
+  public String url;
   public String title;
-  public String content;
   
+  public String insertDateString;
+  public ArrayList<NoticeContentBean> contents = new ArrayList<NoticeContentBean>();
+  
+  
+  public void setJSONObject(JSONObject obj)
+  {
+    try
+    {
+      isTop = obj.getInt("isTop");
+      idx = obj.getString("idx");
+      url = obj.getString("url");
+      title = obj.getString("title");
+      insertDateString = obj.getString("insertDate");
+      try
+      {
+        JSONArray contentArr = obj.getJSONArray("contents");
+        for (int i = 0; i < contentArr.length(); i++)
+        {
+          JSONObject object = contentArr.getJSONObject(i);
+          NoticeContentBean bean = new NoticeContentBean();
+          if (object.has("text"))
+          {
+            bean.text = object.getString("text");
+            contents.add(bean);
+          }
+          if (object.has("image"))
+          {
+            bean.isText = false;
+            bean.text = object.getString("image");
+            contents.add(bean);
+          }
+        }
+      }
+      catch (Exception e)
+      {
+        e.printStackTrace();
+      }
+      
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
+  
+  public class NoticeContentBean
+  {
+    public String text;
+    public boolean isText = true;
+  }
 }
