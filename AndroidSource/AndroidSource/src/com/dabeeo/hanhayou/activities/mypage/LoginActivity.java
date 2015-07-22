cb34d9c00e7144c5c3fe88648a5f99adf7ce8484
donghyunkwon <donghyun.kwon@dabeeo.com>
2015-07-22 18:05:17 +0900

    빈 일정일 경우 보여지는 화면 버튼 strok 값 변경

--- AndroidSource/AndroidSource/src/com/dabeeo/hanhayou/activities/mypage/LoginActivity.java
+++ AndroidSource/AndroidSource/src/com/dabeeo/hanhayou/activities/mypage/LoginActivity.java
@@ -10,7 +10,6 @@ import org.json.JSONObject;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -30,7 +29,6 @@ import com.dabeeo.hanhayou.managers.AlertDialogManager.AlertListener;
 import com.dabeeo.hanhayou.managers.PreferenceManager;
 import com.dabeeo.hanhayou.managers.network.ApiClient;
 import com.dabeeo.hanhayou.managers.network.NetworkResult;
-import com.dabeeo.hanhayou.map.BlinkingCommon;
 import com.dabeeo.hanhayou.views.LoginBottomAlertView;
 
 public class LoginActivity extends Activity
