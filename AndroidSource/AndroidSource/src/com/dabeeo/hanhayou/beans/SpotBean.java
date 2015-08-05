1718ef5070bf01f1bb8eb5fdb92fe6010a584775
donghyunkwon <donghyun.kwon@dabeeo.com>
2015-08-05 11:47:36 +0900

    추가 스트링 적용

--- AndroidSource/AndroidSource/src/com/dabeeo/hanhayou/beans/SpotBean.java
+++ AndroidSource/AndroidSource/src/com/dabeeo/hanhayou/beans/SpotBean.java
@@ -6,6 +6,10 @@ import org.json.JSONObject;
 
 public class SpotBean implements Serializable
 {
+    /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
     public String dayNum;
     public int type;
     public int slotNum;
